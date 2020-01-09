ec2962072ae294f1747e9c00b111a4ac49cddf30
David Hepworth <davidhepp2@gmail.com>
2020-01-09 21:33:19 +0000

    refactor for readability

--- day_1.cpp
+++ day_1.cpp
@@ -4,13 +4,21 @@
 #include <iterator>
 #include <numeric>
 
+auto fuel_for_mass(int const mass)
+{
+  return static_cast<int>(std::floor(mass / 3.0)) - 2;
+}
+
 int main()
 {
-  std::cout << std::accumulate(
-      std::istream_iterator<int>{std::cin},
-      std::istream_iterator<int>{},
-      0,
-      [] (auto const total_fuel, auto const mass) { return total_fuel + std::floor(mass / 3.0) - 2; }
-  ) << std::endl;
+  auto const accumulate_fn = [] (auto const total_fuel, auto const module_mass) {
+    return total_fuel + fuel_for_mass(module_mass);
+  };
+
+  auto const input_iter = std::istream_iterator<int>{std::cin};
+  auto const input_end  = std::istream_iterator<int>{};
+  auto const total_fuel = std::accumulate(input_iter, input_end, 0, accumulate_fn);
+
+  std::cout << total_fuel << std::endl;
   return 0;
 }
